cc0d8c93ffff7798b39d78ae6a74093e0fbf24c9
fareed <fareed.osman152@gmail.com>
2025-05-04 05:34:29 +0300

    Create DB with Identity framework, just basics

---
+++ TaskManagmentSystem/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskManagmentSystem.Models
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
--- TaskManagmentSystem/Program.cs
+++ TaskManagmentSystem/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskManagmentSystem.Models;
+
 namespace TaskManagmentSystem
 {
     public class Program
@@ -8,6 +12,14 @@ namespace TaskManagmentSystem
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("cs"));
+            });
 
             var app = builder.Build();
 
@@ -24,6 +36,7 @@ namespace TaskManagmentSystem
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
