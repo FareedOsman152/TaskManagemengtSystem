74fa2de90a3ad9146a1ef761c985b487553cdb44
Fareed_Osman <fareed.osman152@gmail.com>
2025-05-06 04:29:20 +0300

    SaveRegister action logic with usermanager and signinmanager save in db create cookies --- before test

--- TaskManagmentSystem/Controllers/AccountController.cs
+++ TaskManagmentSystem/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagmentSystem.Models;
+using TaskManagmentSystem.ViewModel;
 
 namespace TaskManagmentSystem.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -11,10 +23,30 @@ namespace TaskManagmentSystem.Controllers
         }
 
         [HttpPost]
-        public IActionResult SaveRegister()
+        public async Task<IActionResult> SaveRegister(UserRegisterViewModel userFromRequest)
         {
-            //logic
-            return View("Register");
+            if(ModelState.IsValid)
+            {
+                var user = new AppUser();
+                user.UserName = userFromRequest.Username;
+                user.Email = userFromRequest.Email;
+                IdentityResult saveResult =
+                    await _userManager.CreateAsync(user,userFromRequest.Password);
+                if(saveResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    // spacific view
+                    return View();
+                }
+                else
+                {
+                    foreach (var error in saveResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View("Register",userFromRequest);
         }
     }
 }
