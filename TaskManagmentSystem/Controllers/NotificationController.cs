483babb7b2314529e8b04ff8d9dbd9edd2888285
Fareed_Osman <fareed.osman152@gmail.com>
2025-06-03 00:30:15 +0300

    Notification controller + show + mark read + mark all read

---
+++ TaskManagmentSystem/Controllers/NotificationController.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagmentSystem.Models;
+using TaskManagmentSystem.ViewModels;
+
+namespace TaskManagmentSystem.Controllers
+{
+    [Authorize]
+    public class NotificationController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NotificationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Show()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var notifications = _context.Notifications
+                .Where(n=>n.AppUserId==userId)
+                .Select(n=>new NotificationsViewModel
+                {
+                    Id = n.Id,
+                    IsRead = n.IsRead,
+                    Details = n.Details,
+                    DateCeated = n.DateCeated
+                })
+                .ToList();
+
+            return View("Show",notifications);
+        }
+
+        public async Task<IActionResult> MakeRead(int Id)
+        {
+            var notification = await _context.Notifications
+                .FirstOrDefaultAsync(n => n.Id == Id);
+            if (notification is null)
+                return NotFound();
+            else
+            {
+                notification.IsRead = true;
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Show");
+            }
+        }
+
+        public async Task<IActionResult> MakeAllRead()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var unreadNotifications = _context.Notifications
+                .Where(n=>n.AppUserId == userId)
+                .Where(n => n.IsRead == false);
+            await unreadNotifications.ForEachAsync(n => n.IsRead = true);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ TaskManagmentSystem/ViewModels/NotificationsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TaskManagmentSystem.Models;
+
+namespace TaskManagmentSystem.ViewModels
+{
+    public class NotificationsViewModel
+    {
+        public int Id { get; set; }
+        public String Details { get; set; } = null!;
+        public DateTime DateCeated { get; set; } = DateTime.Now;
+        public bool IsRead { get; set; }
+
+    }
+}
