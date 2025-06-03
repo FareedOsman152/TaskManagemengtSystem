d175dce40793821caeb6b19da4f8796dccedaf66
Fareed_Osman <fareed.osman152@gmail.com>
2025-06-03 23:52:38 +0300

    create notifi manager => factory and service just for create begin and end notifi

---
+++ TaskManagmentSystem/NotificationsManager/Interfaces/INotificationFactory.cs
@@ -0,0 +1,9 @@
+﻿using TaskManagmentSystem.Models;
+
+namespace TaskManagmentSystem.NotificationsManager.Interfaces
+{
+    public interface INotificationFactory
+    {
+        Notification TaskSchedule(string userId, int userTaskId, string details, bool IsRead = false);
+    }
+}
---
+++ TaskManagmentSystem/NotificationsManager/NotificationFactory.cs
@@ -0,0 +1,19 @@
+﻿using TaskManagmentSystem.Models;
+using TaskManagmentSystem.NotificationsManager.Interfaces;
+
+namespace TaskManagmentSystem.NotificationsManager
+{
+    public class NotificationFactory : INotificationFactory
+    {
+        public Notification TaskSchedule(string userId, int userTaskId, string details, bool IsRead = false)
+        {
+            return new Notification
+            {
+                AppUserId = userId,
+                UserTaskId = userTaskId,
+                Details = details,
+                IsRead = IsRead
+            };
+        }
+    }
+}
