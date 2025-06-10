239c1e3350dff617872eb0899e37822a973f9881
Fareed_Osman <fareed.osman152@gmail.com>
2025-06-10 21:39:22 +0300

    add IsCompeletedin UserTasks Table in DB

---
+++ TaskManagmentSystem/Migrations/20250609132422_add isCompeleted to tasks table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagmentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class addisCompeletedtotaskstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "UserTasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "UserTasks");
+        }
+    }
+}
---
+++ TaskManagmentSystem/Migrations/20250609133551_change iscomleted to isDone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagmentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeiscomletedtoisDone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsCompleted",
+                table: "UserTasks",
+                newName: "IsDone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDone",
+                table: "UserTasks",
+                newName: "IsCompleted");
+        }
+    }
+}
