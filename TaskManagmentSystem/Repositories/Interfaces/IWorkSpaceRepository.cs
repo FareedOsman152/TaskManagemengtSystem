726717a81ccf7e6b6a3dd10d907af2da3bdaac28
Fareed_Osman <fareed.osman152@gmail.com>
2025-06-29 08:46:48 +0300

    init WirkSpace repository

---
+++ TaskManagmentSystem/Repositories/Interfaces/IWorkSpaceRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskManagmentSystem.Helpers;
+using TaskManagmentSystem.Models;
+
+namespace TaskManagmentSystem.Repositories.Interfaces
+{
+    public interface IWorkSpaceRepository
+    {
+        Task<OperationResult<WorkSpace>> GetByIdAsync(int id);
+        Task<OperationResult> CreateAsync(WorkSpace workSpace);
+        Task<OperationResult> UpdateAsync(WorkSpace workSpace);
+        Task<OperationResult> DeleteAsync(int id);
+        Task<OperationResult<List<WorkSpace>>> GetForUserAsync(string userId);
+    }
+}
---
+++ TaskManagmentSystem/Repositories/WorkSpaceRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagmentSystem.Helpers;
+using TaskManagmentSystem.Models;
+using TaskManagmentSystem.Repositories.Interfaces;
+
+namespace TaskManagmentSystem.Repositories
+{
+    public class WorkSpaceRepository : IWorkSpaceRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly Logger<WorkSpaceRepository> _logger;
+        public WorkSpaceRepository(AppDbContext context, Logger<WorkSpaceRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public async Task<OperationResult<WorkSpace>> GetByIdAsync(int id)
+        {
+            var workSpace = await _context.WorkSpaces.FindAsync(id);
+            if (workSpace == null)
+                return OperationResult<WorkSpace>.Failure("WorkSpace not found");
+            return OperationResult<WorkSpace>.Success(workSpace);
+        }
+        public async Task<OperationResult> CreateAsync(WorkSpace workSpace)
+        {
+            _context.WorkSpaces.Add(workSpace);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while creating WorkSpace with Title: {Title}", workSpace.Title);
+                return OperationResult.Failure("An error occurred while creating the WorkSpace: " + ex.Message);
+            }
+            
+            return OperationResult.Success();
+        }
+        public async Task<OperationResult> UpdateAsync(WorkSpace workSpace)
+        {
+            _context.WorkSpaces.Update(workSpace);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while updating WorkSpace with Title: {Title}", workSpace.Title);
+                return OperationResult.Failure("An error occurred while updating the WorkSpace: " + ex.Message);
+            }
+            return OperationResult.Success();
+        }
+        public async Task<OperationResult> DeleteAsync(int id)
+        {
+            var workSpace = await _context.WorkSpaces.FindAsync(id);
+            if (workSpace == null)
+                return OperationResult.Failure("WorkSpace not found");
+            _context.WorkSpaces.Remove(workSpace);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while Deleting WorkSpace with Title: {Title}", workSpace.Title);
+                return OperationResult.Failure("An error occurred while Deleting the WorkSpace: " + ex.Message);
+            }
+            return OperationResult.Success();
+        }
+        public async Task<OperationResult<List<WorkSpace>>> GetForUserAsync(string userId)
+        {
+            var workSpaces = await _context.WorkSpaces
+                .Where(ws => ws.AppUserId == userId)
+                .ToListAsync();
+            return OperationResult<List<WorkSpace>>.Success(workSpaces);
+        }
+     
+    }
+}
